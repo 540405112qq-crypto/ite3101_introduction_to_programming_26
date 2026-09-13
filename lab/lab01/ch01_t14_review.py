0aac79327aaf45410a564cf6bb8f58eb6899a916
540405112qq-crypto <540405112qq@gmail.com>
2026-09-13 12:38:06 +0000

    [skip actions] lab/lab01/ch01_t14_review.py

--- lab/lab01/ch01_t14_review.py
+++ lab/lab01/ch01_t14_review.py
@@ -2,4 +2,5 @@ skill_completed = "Python Syntax"
 exercises_completed = 13
 
 point_total =100
-point_total += exercises_completed * points_per_exer
+point_total += exercises_completed * points_per_exercise
+
