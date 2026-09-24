2e0da234ab9ee251f39007faee0526c5b1e3548c
540405112qq-crypto <540405112qq@gmail.com>
2026-09-24 08:58:11 +0000

    [skip actions] lab/lab05/ch05_t07_word_up.py

--- lab/lab05/ch05_t07_word_up.py
+++ lab/lab05/ch05_t07_word_up.py
@@ -3,6 +3,8 @@ pyg = 'ay'
 original = input('Enter a word:')
 
 if len(original) > 0 and original.isalpha():
-    word = original
+    word = original.lower(
+        fi
+    )
 else:
     print('empty')
